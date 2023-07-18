d507ea9fe4b1af2b2beb6e3c26e18ddf1c344f8f
utfv <21307046@utfv.edu.mx>
2023-07-18 16:29:11 -0600

    17072023

---
+++ app/php/insertar.php
@@ -0,0 +1,13 @@
+<?php 
+error_reporting(e_all ^ e_deprecated);
+header("content-type: text/html; charset=utf-8");
+
+$variable1 = 1234;
+$variable2 ="Eliel";
+
+echo $variable1 .'<br>';
+echo $variable2 .'<br>';
+
+
+
+?>
